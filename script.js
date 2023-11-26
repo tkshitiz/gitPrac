9e09a5de619bfee669a34842bca700458f377fe8
kstha022 <kshitiz.thapa@javra.com>
2023-11-26 20:39:30 +0545

    simple test for opening commit editmsg file

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Hello Worl")
 })
 
-app.listen(3000, () => {
+app.listen(3000, () => { 
     console.log("App listening on port 3000")
 })
 // 
 // added commit 
 //kei changes
 // checking my changes to  other branches
+
+dsjkfhdskjfdskf
